122933127a764769f8724d8e29e0fc6f3a6ab94f
n18012 <n18012@std.it-college.ac.jp>
2018-07-10 16:58:14 +0900

    0705

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -3,16 +3,19 @@
 bear_p = 200
 side_p = 100
 chiken_p = 100
+
 # 個数
 bear_v = 2
 side_v = 1
 chiken_v = 2
+
 # 割引率
 rate = 0.2
 # ポイント割引
 point = 150
+
 # 計算
 payment = chiken_p * (1 - rate)
-sum_v = (bear_p * bear_v) + (side_p * side_v) + payment * chiken_v - point
+sum_v = (bear_p * bear_v) + (side_p * side_v) + (payment * chiken_v) - point
 # 結果を表示
 print("買い物の合計は", sum_v, "円")
