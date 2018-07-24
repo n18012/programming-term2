3aef06799ac623fa9ec5cf05f20badf1b34cf91a
n18012 <n18012@std.it-college.ac.jp>
2018-07-24 09:12:59 +0900

    課題

--- src/basic-c3/0718/task20180718.py
+++ src/basic-c3/0718/task20180718.py
@@ -1,21 +1,66 @@
-# 注文プログラム
-# メインメニューを辞書型で定義
-menu = {'DripCoffee': 280, 'ColdBrewCoffee': 320, 'CafeLatte': 330, 'SoyLatte': 380, 'Cappuccino': 330, 'CaramelFrappuccino': 470, 'MacchaCreamFrappuccino': 470}
+menu = {
+    'DripCoffee': 280,
+    'ColdBrewCoffee': 320,
+    'CafeLatte': 330,
+    'SoyLatte': 380,
+    'Cappuccino': 330,
+    'CaramelFrappuccino': 470,
+    'MacchaCreamFrappuccino': 470,
+}
 
-# オプションを辞書型で定義
-option = {'LowFatMilk': 0, 'NonFatMilk': 0, 'SoyMilk': 50, 'DeepCoffee': 60, 'WhipCream': 70, 'CaramelShrup': 60, 'ChocoSource': 0, 'DeCafe': 50}
+option = {
+    'LowFatMilk': 0,
+    'NonFatMilk': 0,
+    'SoyMilk': 50,
+    'DeepCoffee': 60,
+    'WhipCream': 70,
+    'CaramelShrup': 60,
+    'ChocoSource': 0,
+    'DeCafe': 50,
+}
+# 値を入れる空のリスト
+menu_v = []
+menu_f = []
 
-# 注文内容のリスト
-naiyo = []
-# メニューを入力
 while True:
-    name = input("メインメニューを選んで下さい :")
-    if name == "" or name == "q":
-        print("キャンセルされました")
-        exit()
-    if name in naiyo:
-        print("メインメニューを承りました。")
+    user = input('メインメニューを入力してください>>>')
+    if user in menu.keys():
+        menu_v.append(user)
+        menu_f.append(menu[user])
+        break
+    elif user == 'q' or user == '':
+        print('注文をキャンセルします')
+        exit()   # qとenterが押されれば強制終了
     else:
-        print("選択されたメニューはありません")
-# 合計金額
+        print('存在しないメニューです。')
 
+print('メインメニューを承りました。')
+
+while True:
+    user_option = input('オプションメニューを選んでください>>>')
+    if user_option in option.keys():
+        menu_v.append(user_option)
+        menu_f.append(option[user_option])
+        break
+    elif user_option == 'q' or user_option == '':
+        print('注文内容は{}です'.format(menu_v))
+        print('合計金額は{}円です。右奥のカウンターでお待ちください'.format(menu_f))
+        exit()
+    else:
+        print('存在しないメニューです')
+        continue
+# 表示内容を変えてループさせるために新しいループを作成
+while True:
+    user_option2 = input('他にオプションメニューの注文はございますか？>>>')
+    if user_option2 in option.keys():
+        menu_v.append(user_option2)
+        menu_f.append(option[user_option2])
+    elif user_option2 == 'q' or user_option2 == '':
+        break
+    else:
+        print('存在しないメニューです')
+
+# sum関数でappendで入れてきた値を足す
+pay = sum(menu_f)
+print('注文内容は{}です'.format(menu_v))
+print('合計金額は{}円です。右奥のカウンターでお待ちください'.format(pay))
