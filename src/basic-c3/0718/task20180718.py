3b125dc1a67d4f0d172eecd464bbdd0f5c4c20fa
n18012 <n18012@std.it-college.ac.jp>
2018-08-01 12:26:08 +0900

    課題

--- src/basic-c3/0718/task20180718.py
+++ src/basic-c3/0718/task20180718.py
@@ -18,15 +18,16 @@ option = {
     'ChocoSource': 0,
     'DeCafe': 50,
 }
+
 # 値を入れる空のリスト
 menu_v = []
-menu_f = []
+menu_f = 0
 
 while True:
     user = input('メインメニューを入力してください>>>')
     if user in menu.keys():
         menu_v.append(user)
-        menu_f.append(menu[user])
+        menu_f += menu[user]
         break
     elif user == 'q' or user == '':
         print('注文をキャンセルします')
@@ -40,7 +41,7 @@ while True:
     user_option = input('オプションメニューを選んでください>>>')
     if user_option in option.keys():
         menu_v.append(user_option)
-        menu_f.append(option[user_option])
+        menu_f += option[user_option]
         break
     elif user_option == 'q' or user_option == '':
         print('注文内容は{}です'.format(menu_v))
@@ -49,12 +50,13 @@ while True:
     else:
         print('存在しないメニューです')
         continue
+
 # 表示内容を変えてループさせるために新しいループを作成
 while True:
     user_option2 = input('他にオプションメニューの注文はございますか？>>>')
     if user_option2 in option.keys():
         menu_v.append(user_option2)
-        menu_f.append(option[user_option2])
+        menu_f += option[user_option2]
     elif user_option2 == 'q' or user_option2 == '':
         break
     else:
@@ -62,5 +64,6 @@ while True:
 
 # sum関数でappendで入れてきた値を足す
 pay = sum(menu_f)
-print('注文内容は{}です'.format(menu_v))
+pay2 = sum(menu_v)
+print('注文内容は{}です'.format(pay2))
 print('合計金額は{}円です。右奥のカウンターでお待ちください'.format(pay))
